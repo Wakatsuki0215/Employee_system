29cafebbe93d959ec6992723e05a078c43bf4d70
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-11-11 16:04:04 +0900

    login add

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * 認証の試行を処理
+     */
+
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        $request_base = $request->all();
+        $credentials = [
+            'mail' => $request['email'],
+            'password' => $request['password']
+        ];
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('list');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
