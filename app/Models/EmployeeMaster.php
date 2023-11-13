e94c76551f9e0c060dbb2abef373fabd13535ccb
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-11-13 15:04:51 +0900

    fillable update

---
+++ app/Models/EmployeeMaster.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class EmployeeMaster extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'kana',
+        'gender',
+        'age',
+        'postcode',
+        'address',
+        'affiliation_id',
+        'mail',
+        'tel',
+        'role',
+        'status',
+        'password',
+        'created_by',
+        'created_at',
+        'updated_by',
+        'updated_at',
+        // DBから
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
