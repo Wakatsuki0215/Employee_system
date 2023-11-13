b7ce84752a6816dec28bb19d9d22e82e9fa5a174
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-11-13 14:49:14 +0900

    login update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,12 +9,22 @@
 </head>
 <body>
     <h1>ログイン画面</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
     <form action={{ url('/login') }} method="post">
         @csrf
-        <label for="email">メールアドレス</label>
-        <input type="email" name="email" id="email">
+        <label for="id">社員番号</label>
+        <input type="text" name="id" id="id"><br>
         <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="password" id="password"><br>
         <button type="submit">送信</button>
     </form>
 </body>
