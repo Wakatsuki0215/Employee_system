db0e881207bd76fe802de021f5e686be67f3dc89
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-11-17 18:57:05 +0900

    sesstion情報取得　編集

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,27 +16,50 @@ class LoginController extends Controller
 
     public function login(LoginRequest $request): RedirectResponse
     {
-        $request_base = $request->all();
         $credentials = [
             'id' => $request['id'],
             'password' => $request['password']
         ];
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('list');
+
+            $data = $request->session()->all();
+
+            // $user = Auth::id();
+            // $user = Auth::getName();
+            // //session データ取得（ID,name,affiliation,role）
+            // $request->session()->get([
+            //     'id' => 'id',
+            //     'name' => 'name',
+            //     'affiliation_id' => 'affiliation_id',
+            //     'role' => 'role',
+            // ]);
+
+            // //session データ保存
+            // $request->session()->put([
+            //     'id' => 'id',
+            //     'name' => 'name',
+            //     'affiliation_id' => 'affiliation_id',
+            //     'role' => 'role',
+            // ]);
+
+            return redirect('employee_list');
         }
 
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ])->onlyInput('email');
     }
- 
+
     public function logout(Request $request): RedirectResponse
     {
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerate();
+
+        //session データ削除
+        $request->session()->flush();
+
         return redirect('login');
     }
-    
 }
