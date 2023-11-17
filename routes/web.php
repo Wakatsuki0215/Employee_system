821d3f09d16eaba060423f5e4121245f8ebdce78
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-11-17 15:53:43 +0900

    ログイン認証　グループ化

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,30 @@ Route::get('/login', function () {
 
 Route::post('/login',[LoginController::class,'login']);
 
+// //一覧
+// Route::get('/employee_list',[EmployeeController::class,'index'])->name('index')->middleware('auth');
+// //登録
+// Route::get('/employee_add',[EmployeeController::class,'new'])->name('new')->middleware('auth');
+// Route::post('/employee_add',[EmployeeController::class,'create'])->name('create')->middleware('auth');
+// //詳細
 
-Route::middleware('auth')->group(function ()
-{
-    Route::get('/employee_list',[EmployeeController::class,'index'])->name('index');
-           
-    Route::post('/logout',[LoginController::class,'logout']); //ログアウト
+// //編集
+
+// //ログアウト
+// Route::get('/logout',[LoginController::class,'logout']);
+
+
+// ログイン認証　グループroute
+Route::group(['middleware' => 'auth'],function(){
+	//一覧
+	Route::get('/employee_list',[EmployeeController::class,'index'])->name('index');
+	//登録
+	Route::get('/employee_add',[EmployeeController::class,'new'])->name('new');
+	Route::post('/employee_add',[EmployeeController::class,'create'])->name('create');
+	//詳細
+
+	//編集
+
+	//ログアウト
+	Route::get('/logout',[LoginController::class,'logout']);
 });
-
-
-
