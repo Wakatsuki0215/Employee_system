61b918d3cd4db8784c6a794eb974ec90d4577baa
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-12-01 18:25:29 +0900

    affiliationのモデル作成

---
+++ app/Models/AffiliationMaster.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AffiliationMaster extends Model
+{
+    use HasFactory;
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'affiliation_name',
+        'created_by',
+        'created_at',
+        'updated_by',
+        'updated_at',
+        'deleted_by',
+        'deleted_at',
+    ];
+}
