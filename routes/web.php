9d454bb830343c1dbf54b50c955f4bebec5a8fff
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-12-08 13:43:54 +0900

    auth 削除

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,26 @@ use App\Http\Controllers\LogoutController;
 */
 
 // ログイン
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('/login', function () {return view('login');});
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
-Route::post('/login',[LoginController::class,'login']);
+//一覧
+Route::get('/employee_list', [EmployeeController::class, 'index'])->name('index');
+Route::put('/employee_list', [EmployeeController::class, 'index'])->name('index');
 
+//登録
+Route::get('/employee_add', [EmployeeController::class, 'new'])->name('new');
+Route::post('/employee_add', [EmployeeController::class, 'create'])->name('create');
 
+//詳細
+Route::get('/employee_show', [EmployeeController::class, 'show'])->name('show');
 
-// ログイン認証　グループroute
-Route::group(['middleware' => 'auth'],function(){
-	//一覧
-	Route::get('/employee_list',[EmployeeController::class,'index'])->name('index');
-	Route::put('/employee_list',[EmployeeController::class,'index'])->name('index');
+//編集
+Route::get('/employee_edit/{id}', [EmployeeController::class, 'get'])->name('get');
+Route::put('/employee_edit/{id}', [EmployeeController::class, 'update'])->name('update');
 
-	//登録
-	Route::get('/employee_add',[EmployeeController::class,'new'])->name('new');
-	Route::post('/employee_add',[EmployeeController::class,'create'])->name('create');
+// パスワード変更
+Route::put('/employee_password/{id}', [EmployeeController::class, 'password'])->name('password_update');
 
-	//詳細
-	Route::get('/employee_show',[EmployeeController::class,'show'])->name('show');
-
-	//編集
-	Route::get('/employee_edit/{id}',[EmployeeController::class,'get'])->name('get');
-	Route::put('/employee_edit/{id}',[EmployeeController::class,'update'])->name('update');
-
-	//ログアウト
-	Route::get('/logout',[LoginController::class,'logout'])->name('logout');
-});
+//ログアウト
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
