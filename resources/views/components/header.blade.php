aaba41ab92102906687b4cbc5ca043d57ea372b7
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-12-08 14:21:44 +0900

    header修正

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,6 +4,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="{{ asset('/css/header.css') }}">
 
 <header>
   <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#e3f2fd;">
@@ -13,10 +14,10 @@
         <li class="nav-item dropdown">
           ログインユーザー：
           <a class="text-muted" href="#" id="navbarSupportedContent" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            {{ Auth::user()->name; }}<!-- ログインユーザーの名前表示 -->
+            {{ session('name') }}
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
               <a class="dropdown-item" href={{ url('/employee_show') }}>マイページ</a>
-              <a class="dropdown-item" href="#">パスワード</a>
+              <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal{{ session('id') }}">パスワード変更</a>
               <a class="dropdown-item" href="/logout">ログアウト</a>
             </div>
         </li>
