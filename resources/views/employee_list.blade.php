9e6b3a82db1c336fc35006de1765c48a4b5345fc
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-12-08 15:57:42 +0900

    css 調整

--- resources/views/employee_list.blade.php
+++ resources/views/employee_list.blade.php
@@ -10,101 +10,146 @@
 
 
 <h4>社員名簿一覧</h4>
+@if (session('flash_message'))
+<div class="alert" style="color: green;">
+    {{ session('flash_message') }}
+</div>
+@endif
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+</div>
+@endif
 <!-- 検索条件　入力フォーム -->
 <div class="container d-flex align-items-center justify-content-center p-3 mb-2 bg-light text-black">
-  <form action={{url('/employee_list')}} method="GET">
-    <!-- 名前検索 -->
-    <label for="name">名前
-      <input type="text" name="name">
-    </label>
+    <form action={{url('/employee_list')}} method="GET">
+        <!-- 名前検索 -->
+        <label for="name">名前
+            <input type="text" name="name">
+        </label>
+        <!-- enumで配列を取ってくるようにする -->
+        <!-- 性別検索 -->
+        <label for="gender">性別
+            <select name="gender" id="gender">
+                @foreach (\App\Enums\Gender::getGenders() as $value => $label)
+                <option value="{{ $value }}">{{ $label }}</option>
+                @endforeach
+            </select>
+        </label>
+        <!-- データベースから取ってくる -->
+        <!-- 所属検索 -->
+        <label for="affiliation_id">所属
+            <select name="affiliation_id" id="affiliation_id">
+                <option value=""></option>
+                @foreach ($response['affiliations'] as $item)
+                <option value="{{ $item->id }}">{{ $item->affiliation_name }}</option>
+                @endforeach
+            </select>
+        </label>
 
-    <!-- 性別検索 -->
-    <label for="gender">性別
-      <select name="gender" id="gender">
-        <option value=""></option>
-        <option value="male">男性</option>
-        <option value="female">女性</option>
-      </select>
-    </label>
+        <!-- 権限検索 -->
+        <label for="role">権限
+            <select name="role" id="role">
+                @foreach (\App\Enums\Role::getRoles() as $value => $label)
+                <option value="{{ $value }}">{{ $label }}</option>
+                @endforeach
+            </select>
+        </label>
 
-    <!-- 所属検索 -->
-    <label for="affiliation_id">所属
-      <select name="affiliation_id" id="affiliation_id">
-        <option value=""></option>
-        <option value="1">システム事業部</option>
-        <option value="2">BS</option>
-        <option value="3">CS</option>
-        <option value="4">営業部</option>
-        <option value="5">総務部</option>
-        <option value="6">業務本部</option>
-      </select>
-    </label>
+        <!-- 無効社員　チェック項目 -->
+        <label for="status">
+            <input type="checkbox" name="status" value="disabled">
+            無効を含む
+        </label>
 
-    <!-- 権限検索 -->
-    <label for="role">権限
-      <select name="role" id="role">
-        <option value=""></option>
-        <option value="general">一般</option>
-        <option value="admin">管理者</option>
-      </select>
-    </label>
-
-    <!-- 無効社員　チェック項目 -->
-    <label for="status">
-      <input type="checkbox" name="status" value="disabled">
-      無効を含む
-    </label>
-
-    <!-- クリア・検索ボタン -->
-    <input type=reset class="btn btn-secondary" value="クリア">
-    <input type=submit class="btn btn-primary" value="検索">
-  </form>
+        <!-- クリア・検索ボタン -->
+        <input type=reset class="btn btn-secondary" value="クリア">
+        <input type=submit class="btn btn-primary" value="検索">
+    </form>
 </div>
 
 <!-- 新規登録画面　遷移ボタン -->
 <div class="container">
-  <div class="row justify-content-between">
-    <div class="count">
-      {{ $employees->firstItem() }}~{{ $employees->lastItem() }}件表示/全{{ $employees->total() }}件
+    <div class="row justify-content-between">
+        <div class="count">
+            {{ $employees->firstItem() }}~{{ $employees->lastItem() }}件表示/全{{ $employees->total() }}件
+        </div>
+        <div class="add-btn">
+            <form action={{ url('/employee_add')}} method="get">
+                <input type=submit class=" btn btn-primary" value="新規作成">
+            </form>
+        </div>
     </div>
-    <div class="add-btn">
-      <form action={{ url('/employee_add')}} method="get">
-        <input type=submit class=" btn btn-primary" value="新規作成">
-      </form>
-    </div>
-  </div>
 </div>
 
 <!-- 社員一覧表示 -->
-<div class="container scroll_area">
-  <table class="table  table-striped">
-    <tr>
-      <th>名前</th>
-      <th>性別</th>
-      <th>所属</th>
-      <th>メールアドレス</th>
-      <th>電話番号</th>
-      <th></th>
-      <th></th>
-    </tr>
-
-    @foreach ($response['employees'] as $employee)
-    <tr>
-      <td>{{ $employee->name }}</td>
-      <td>{{ \App\Enums\Gender::getDescription($employee->gender) }}</td>
-      <td>{{ \App\Enums\Affiliation::getDescription($employee->affiliation_id) }}</td>
-      <td>{{ $employee->mail }}</td>
-      <td>{{ $employee->tel }}</td>
-
-      <!-- パスワードモーダル -->
-      <td>
-        <a href="#" class="bi bi-key modal-open" style="font-size: 1.5rem; color: gray"></a>
-        @include('components.password_modal')
-      </td>
-      <td><a href="/employee_edit/{{$employee->id}}" class="bi bi-pencil" style="font-size: 1.5rem; color: green;"></a></td>
-    </tr>
-    @endforeach
-  </table>
+<div class="container">
+    <div class="scroll_area">
+        <table class="table  table-striped">
+            <tr>
+                <th>名前</th>
+                <th>性別</th>
+                <th>所属</th>
+                <th>メールアドレス</th>
+                <th>電話番号</th>
+                <th></th>
+                <th></th>
+            </tr>
+            @foreach ($response['employees'] as $employee)
+            <tr>
+                <td>{{ $employee->name }}</td>
+                <!-- インクルートで -->
+                <td>{{ \App\Enums\Gender::getGender($employee->gender) }}</td>
+                <!-- ファンクションを作って持ってくるようにする。 -->
+                <td>{{ \App\Enums\Affiliation::getDescription($employee->affiliation_id) }}</td>
+                <!-- @inject('response','App\Http\Services\GetEmployeeListService')　-->
+                <td>{{ $employee->mail }}</td>
+                <td>{{ $employee->tel }}</td>
+                <!-- パスワードモーダル -->
+                <td>
+                    <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal{{ $employee->id }}">
+                        <i class="bi-key" style="font-size: 1.5rem;"></i>
+                    </button>
+                    <!-- Modal -->
+                    <div class="modal" id="exampleModal{{ $employee->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="dialog" role="document">
+                            <div class="content">
+                                <div>
+                                    <h5 class="modal-title" id="exampleModalLabel"> {{ $employee->name }} のパスワード変更</h5>
+                                </div>
+                                <form class="center" action="{{ url('/employee_password', $employee->id) }}" method="post">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="modal-password-form">
+                                        <label for="password">新しいパスワード</label>
+                                        <input type="password" name="password">
+                                        <i class="toggle-pass bi bi-eye-slash"></i>
+                                    </div>
+                                    <div class="modal-password-form">
+                                        <label for="password">新しいパスワード(確認用)</label>
+                                        <input type="password" name="password_confirmation">
+                                        <i class="toggle-pass bi bi-eye-slash"></i>
+                                    </div>
+                                    <div class="modal-password-form">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">戻る</button>
+                                        <input type="submit" class="btn btn-success" value="変更">
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td><a href="/employee_edit/{{$employee->id}}" class="bi bi-pencil" style="font-size: 1.5rem; color: green;"></a></td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
 
 <!-- <div class="pagination">{{ $employees->links() }}</div> -->
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,7 +9,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<!-- JavaScript Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+	<link rel="stylesheet" href="{{ asset('/css/login.css') }}">
 	<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 	<script src="js/password_modal.js"></script>
 
@@ -25,6 +25,7 @@
 			<h3>社員管理システム</h3>
 		</div>
 	</header>
+
 	<div class="container">
 		<div class="center-block">
 			@if ($errors->any())
@@ -42,31 +43,48 @@
 		<form action={{ url('/login') }} method="post">
 			@csrf
 			<div class="row">
-				<div class="col-5 text-end">
-					<label for="id" class="text-end">社員番号</label>
+				<div class="col-md-5 text-end">
+					<label for="id">社員番号</label>
 				</div>
-				<div class="col-7">
+				<div class="col-md-1"></div>
+				<div class="col-md-5">
 					<input type="text" name="id" id="id">
 				</div>
 			</div>
-			</br>
 			<div class="row">
-				<div class="col-5 text-end">
+				<div class="col-md-5 text-end">
 					<label for="password">パスワード</label>
 				</div>
-				<div class="col-7">
-					<input type="password" name="password" id="password">
+				<div class="col-md-1"></div>
+				<div class="col-md-5">
+					<input type="password" id="password" name="password">
+					<i id="passwordIcon" class="toggle-pass bi bi-eye-slash"></i>
 				</div>
 			</div>
-			</br>
-			<div class="row">
-				<div class="text-center">
-					<button class="btn btn-primary bi-arrow-return-right login-button" type="submit"> ログイン</button>
+			<div class="content">
+				<button class="btn btn-primary login-button" type="submit">
+				<i id="loginIcon" class="bi-arrow-return-right"></i>ログイン</button>
+			</div>
+
+
+			<!-- <div class="content">
+				<div class="col-sm">
+					<label for="id">社員番号</label>
+					<input type="text" name="id" id="id">
 				</div>
 			</div>
+			<div class="content">
+				<div class="col-sm">
+					<label for="password">パスワード</label>
+					<input type="password" id="password" name="password" id="password">
+					<i id="toggleIcon" class="toggle-pass bi bi-eye-slash"></i>
+				</div>
+			</div>
+			<div class="content">
+				<button class="btn btn-primary bi-arrow-return-right login-button" type="submit"> ログイン</button>
+			</div> -->
 		</form>
 	</div>
-
 </body>
 
 </html>
