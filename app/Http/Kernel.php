34bce353c0ef8be5f6d647f96be8bc8532c6d3c9
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-12-08 16:44:17 +0900

    middleware作成に合わせmiddlewareAliases追記

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,10 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        // ログインしている人が閲覧可能
+        'myself' => \App\Http\Middleware\Myself::class,
+        //権限がadminの人のみ操作可能
+        'authority' => \App\Http\Middleware\Authority::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
