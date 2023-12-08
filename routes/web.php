a509027485040c34e7c678b1374e5ddf42e3169b
wakatsuki0125 <wakatsuki-tatsuya@wiznet.co.jp>
2023-12-08 16:42:48 +0900

    group　追加

--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,28 @@ use App\Http\Controllers\LogoutController;
 Route::get('/login', function () {return view('login');});
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
+
 // ログインユーザーのみ遷移できる。
-Route::group(['middleware' => 'myself'], function () {
+Route::group(['middleware' => ['myself']], function () {
+    //一覧
+    Route::get('/employee_list', [EmployeeController::class, 'index'])->name('index');
+    Route::put('/employee_list', [EmployeeController::class, 'index'])->name('index');
 
-//一覧
-Route::get('/employee_list', [EmployeeController::class, 'index'])->name('index');
-Route::put('/employee_list', [EmployeeController::class, 'index'])->name('index');
+    //詳細
+    Route::get('/employee_show', [EmployeeController::class, 'show'])->name('show');
 
-//登録
-Route::get('/employee_add', [EmployeeController::class, 'new'])->name('new');
-Route::post('/employee_add', [EmployeeController::class, 'create'])->name('create');
+    Route::group(['middleware' => ['authority']], function () {
+        //登録
+        Route::get('/employee_add', [EmployeeController::class, 'new'])->name('new');
+        Route::post('/employee_add', [EmployeeController::class, 'create'])->name('create');
 
-//詳細
-Route::get('/employee_show', [EmployeeController::class, 'show'])->name('show');
+        //編集
+        Route::get('/employee_edit/{id}', [EmployeeController::class, 'get'])->name('get');
+        Route::put('/employee_edit/{id}', [EmployeeController::class, 'update'])->name('update');
 
-//編集
-Route::get('/employee_edit/{id}', [EmployeeController::class, 'get'])->name('get');
-Route::put('/employee_edit/{id}', [EmployeeController::class, 'update'])->name('update');
-
-// パスワード変更
-Route::put('/employee_password/{id}', [EmployeeController::class, 'password'])->name('password_update');
+    // パスワード変更
+    Route::put('/employee_password/{id}', [EmployeeController::class, 'password'])->name('password_update');
+    });
 });
 
 //ログアウト
